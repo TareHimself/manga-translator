f064abedc02d8c29806ce0d8d151717c14074e27
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2023-05-05 02:58:48 -0400

    trani script modifications

--- train.py
+++ train.py
@@ -1,5 +1,15 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8m-seg.pt")  # YOLO("yolov8m-seg.pt")
+# detection
+# model = YOLO("yolov8n.pt")  # YOLO("yolov8m-seg.pt")
 
-model.train(data="train_seg.yaml", epochs=70, batch=10, device=0, workers=0)
+# model.train(data="train_detect.yaml", epochs=200, batch=30, device=0, workers=0)
+
+# #segmentation
+# model = YOLO(
+#     "D:\\Github\\manga-translator\\runs\\segment\\train12\\weights\\best.pt"
+# )  # YOLO("yolov8m-seg.pt")
+
+# model.train(
+#     data="train_seg.yaml", epochs=200, batch=30, patience=100, device=0, workers=0
+# )
